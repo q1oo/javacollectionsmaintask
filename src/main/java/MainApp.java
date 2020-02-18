98dcf21855a11ef9cb5626ece54fe372ebb5cbf4
Azamat Ismagilov <azoma06@mail.ru>
2020-02-18 23:56:44 +0500

    add dependency postgresql in pom.xml

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -6,9 +6,8 @@ import static service.FileNames.CARS;
 public class MainApp {
     public static void main(String[] args) {
         TransportStaiton staiton = new TransportStaiton();
-//        System.out.println(staiton.getSumOfTransportPriceFromDatabase());
-//        System.out.println(staiton.sortListOfTransportsByFuelConsumptionFromDatabase(staiton.getListOfAllTransportsFromDatabase()));
-//        System.out.println(staiton.getListOfTransportsByMaxSpeedRangeFromDatabase(0, 160));
-        System.out.println(staiton.getListOfTransportsFromFile(CARS));
+        System.out.println(staiton.getSumOfTransportPriceFromDatabase());
+        System.out.println(staiton.sortListOfTransportsByFuelConsumptionFromDatabase(staiton.getListOfAllTransportsFromDatabase()));
+        System.out.println(staiton.getListOfTransportsByMaxSpeedRangeFromDatabase(0, 160));
     }
 }
